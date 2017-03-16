ba5c9b5c360fee871d2ce253ccaa69c56f67c3fd
zalent <none@none>
2017-03-16 11:13:37 +0200

    Fix attempt for compatibility with openuserjs

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // ==UserScript==
-// @name        khanacademy-disable-sub
+// @name khanacademy-disable-sub
 // @description disable youtube subs by default
-// @namespace   https://gitlab.com/zalent/khanacademy-disable-sub
-// @include     https://www.khanacademy.org/*
-// @version     0.1.0.4
-// @author      liartuw
-// @run-at      document-end
-// @grant       none
-// @downloadURL https://gitlab.com/zalent/khanacademy-disable-sub/raw/master/user.js
-// @updateURL   https://gitlab.com/zalent/khanacademy-disable-sub/raw/master/meta.js
+// @namespace https://gitlab.com/zalent
+// @homepageURL https://gitlab.com/zalent/khanacademy-disable-sub
+// @include https://www.khanacademy.org/*
+// @version 0.1.0.4
+// @license MIT
+// @author rixils
+// @run-at document-end
+// @grant none
+// @downloadURL https://gitlab.com/zalent/khanacademy-disable-sub/raw/master/khanacademy-disable-sub.user.js
+// @updateURL https://gitlab.com/zalent/khanacademy-disable-sub/raw/master/khanacademy-disable-sub.meta.js
 // ==/UserScript==
 (() => {
     addOnChangeEventListenerTo(document.body, () => {
---
+++ khanacademy-disable-sub.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name khanacademy-disable-sub
+// @description disable youtube subs by default
+// @namespace https://gitlab.com/zalent
+// @homepageURL https://gitlab.com/zalent/khanacademy-disable-sub
+// @include https://www.khanacademy.org/*
+// @version 0.1.0.4
+// @license MIT
+// @author rixils
+// @run-at document-end
+// @grant none
+// @downloadURL https://gitlab.com/zalent/khanacademy-disable-sub/raw/master/khanacademy-disable-sub.user.js
+// @updateURL https://gitlab.com/zalent/khanacademy-disable-sub/raw/master/khanacademy-disable-sub.meta.js
+// ==/UserScript==
