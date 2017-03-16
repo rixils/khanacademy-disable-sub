99912b5dc3a55fb5b18d6f0d08f0b24b8eb988ba
zalent <none@none>
2017-03-16 11:19:38 +0200

    Update description

--- khanacademy-disable-sub.meta.js
+++ khanacademy-disable-sub.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name khanacademy-disable-sub
-// @description disable youtube subs by default
+// @description disables youtube subtitles by default
 // @namespace https://gitlab.com/zalent
 // @homepageURL https://gitlab.com/zalent/khanacademy-disable-sub
 // @include https://www.khanacademy.org/*
-// @version 0.1.0.5
+// @version 0.1.0.6
 // @license MIT
 // @author rixils
 // @run-at document-end
